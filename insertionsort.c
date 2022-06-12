30933103cbfd888bf1e58329164a8795e25b9ceb
Rohan Jhaldiyal <rohanjhaldiyal@gmail.com>
2022-06-12 12:59:52 +0530

    updated code

--- insertionsort.c
+++ insertionsort.c
@@ -3,37 +3,33 @@
 void main()
 {
     int a[100], n, i, j, temp;
-    printf("Input size of the array\n");
+    printf("Input size of the array: ");
     scanf("%d", &n);
-    printf("\nInput integer values in the array \n");
+    printf("Input integer values in the array: ");
     for (int i = 0; i < n; i++)
     {
         scanf("%d", &a[i]);
     }
-    printf("\nThe input array is\n");
+    printf("The input array is: ");
     for (i = 0; i < n; i++)
     {
-        printf("%d\n", a[i]);
+        printf("%d ", a[i]);
     }
     for (i = 0; i < n - 1; i++)
     {
-        for (j = i + 1; j > 0; j--)
-        {
-            if (a[j] < a[j - 1])
-            {
+        for (j = i + 1; j > 0; j--) {
+            if (a[j] < a[j - 1]) {
                 temp = a[j];
                 a[j] = a[j - 1];
                 a[j - 1] = temp;
             }
-            else
-            {
+            else      
                 break;
-            }
         }
     }
-    printf("\nThe sorted array is\n");
+    printf("\nThe sorted array is: ");
     for (i = 0; i < n; i++)
     {
-        printf("%d\n", a[i]);
+        printf("%d ", a[i]);
     }
 }
